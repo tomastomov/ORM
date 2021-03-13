58df67afd1f8c299cd4cc553f91cc98ce648e0c3
Tomas Tomov <tomasstomov@gmail.com>
2021-03-13 13:45:22 +0200

    Added playground project and a class library

---
+++ ORM/ORMPlayground/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ORMPlayground
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
