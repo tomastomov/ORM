97c81f82ca50465cacd9f65e3a2a314f4522f236
Tomas Tomov <tomasstomov@gmail.com>
2021-03-24 18:29:35 +0200

    Implementing IQueryable (in progress)

--- ORM/ORM/Contracts/DatabaseTable.cs
+++ ORM/ORM/Contracts/DatabaseTable.cs
@@ -1,10 +1,28 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 
 namespace ORM.Contracts
 {
-    public abstract class DatabaseTable<TEntity>
+    public abstract class DatabaseTable<TEntity> : IQueryable<TEntity>
     {
+        public virtual Type ElementType => throw new NotImplementedException();
+
+        public virtual Expression Expression => throw new NotImplementedException();
+
+        public virtual IQueryProvider Provider => throw new NotImplementedException();
+
+        public virtual IEnumerator<TEntity> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
---
+++ ORM/ORM/Implementation/InternalDatabaseTable.cs
@@ -0,0 +1,37 @@
+﻿using ORM.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace ORM.Implementation
+{
+    internal class InternalDatabaseTable<TEntity> : DatabaseTable<TEntity>, IQueryProvider
+    {
+        public override Expression Expression => Expression.Constant(this);
+
+        public override Type ElementType => typeof(TEntity);
+
+        public override IQueryProvider Provider => this;
+
+        public IQueryable CreateQuery(Expression expression)
+        {
+            
+            return null;
+        }
+
+        public IQueryable<TElement> CreateQuery<TElement>(Expression expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object Execute(Expression expression)
+            => Provider.Execute<object>(expression);
+
+        public TResult Execute<TResult>(Expression expression)
+        {
+            throw new NotImplementedException();
+        }
+        public override IEnumerator<TEntity> GetEnumerator() => ((IEnumerable<TEntity>)Provider.Execute(Expression.Constant(this))).GetEnumerator();
+    }
+}
